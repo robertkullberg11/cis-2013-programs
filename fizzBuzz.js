eed0a854f63494280d213bd3247486343a311b48
robertkullberg11 <112423782+robertkullberg11@users.noreply.github.com>
2022-10-28 13:50:10 -0500

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,28 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+doStuffComputer = function()
+{
+	whileFunction();
+	//forFunction();
+	//doWhileFunction();
+};
+
+
+function whileFunction()
+{
+	var counter = 1;
+	var output = "";	
+};
+
+
+
+
+
+
+window.onload = function()
+{
+	$("doIt").onclick = doStuffComputer;
+}
