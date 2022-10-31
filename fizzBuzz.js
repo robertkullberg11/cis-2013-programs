08665c4a1939535c7d871f0ca81678938fb3c400
robertkullberg11 <112423782+robertkullberg11@users.noreply.github.com>
2022-10-31 11:32:57 -0500

    Add files via upload

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -6,8 +6,8 @@ var $ = function (id)
 doStuffComputer = function()
 {
 	whileFunction();
-	//forFunction();
-	//doWhileFunction();
+	forFunction();
+	doWhileFunction();
 };
 
 
@@ -15,12 +15,93 @@ function whileFunction()
 {
 	var counter = 1;
 	var output = "";	
-};
+    
+    while(counter<=100)
+    {
+        if (counter % 15===0)
+        {
+            output = output + "FizzBuzz\n";
+            counter++;
+        }
+        else if(counter % 5===0)
+        {
+            output = output + "Buzz\n";
+            counter++;
+        }
+        else if(counter % 3===0)
+        {
+            output = output + "Fizz\n";
+            counter++;
+        }
+        else
+        {
+            output = output + counter + "\n";
+            counter++;
+        }
+    }
+    alert(output);
+}
 
+function forFunction()
+{
+	var output = "";	
+    
+    for(var counter = 1; counter <=100; counter++)
+    {
+        if(counter % 15===0)
+        {
+            output = output + "FizzBuzz\n";
+           // counter++;
+        }
+        else if(counter % 5===0)
+        {
+            output = output + "Buzz\n";
+            //counter++;
+        }
+        else if(counter % 3===0)
+        {
+            output = output + "Fizz\n";
+           // counter++;
+        }
+        else
+        {
+            output = output + counter +"\n";
+           // counter++;
+        }
+    }
+    alert(output);
+}
 
-
-
-
+function whileFunction()
+{
+	var counter = 1;
+	var output = "";	
+    
+    do 
+    {
+        if(counter % 15===0) {
+            output = output + "FizzBuzz\n";
+            counter++;
+        }
+        else if(counter % 5===0)
+        {
+            output = output + "Buzz\n";
+            counter++;
+        }
+        else if(counter % 3===0)
+        {
+            output = output + "Fizz\n";
+            counter++;
+        }
+        else
+        {
+            output = output + counter +"\n";
+            counter++;
+        }
+    } while(counter<=100)
+    
+    alert(output);
+}
 
 window.onload = function()
 {
